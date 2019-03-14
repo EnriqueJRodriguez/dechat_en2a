b664206b83812ba3d9ca7ed12f7d2365b02373cf
EnriqueJRodriguez <enriquejrodriguezmartin@gmail.com>
2019-03-15 00:52:15 +0100

    Asking user for credentials, only authentication left.

--- index.js
+++ index.js
@@ -2,23 +2,38 @@ const http = require('http');
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const hostname = '127.0.0.1';
-const port = 3000;
+const inquierer = require('./lib/inquirer');
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello deChat\n')
-})
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello deChat\n');
+// })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`)
+//   callback();
+// })
 
-clear();
-console.log(
-  chalk.blue(
-    figlet.textSync('de-chat', {horizontalLayout: 'full'})
-  )
-);
+
+function startSession() {
+  clear();
+  console.log(
+    chalk.blue(
+      figlet.textSync('de-chat', {horizontalLayout: 'full'})
+    )
+  );
+  askForCredentials();
+}
+
+async function askForCredentials() {
+  const credentials = await inquierer.askSolidCredentials();
+  console.log(credentials.username);
+}
+
+startSession();
+
+
 
